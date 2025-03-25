8bd97f89c7868638ac4cff6e6ac216dc5c9871ea
동영 <arhaxoda@naver.com>
2025-03-25 17:56:42 +0900

    오타수정

--- part3chapter07/app/src/main/java/com/example/part3/chapter07/InputActivity.kt
+++ part3chapter07/app/src/main/java/com/example/part3/chapter07/InputActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.part3.chapter07.databinding.ActivityMainBinding
+import com.example.part3.chapter07.databinding.ActivityInputBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class InputActivity :AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class InputActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInputBinding
 
-    private val viewModel : InputViewModel by viewModels()
+    private val viewModel: InputViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+        binding = ActivityInputBinding.inflate(layoutInflater).apply {
             setContentView(root)
             lifecycleOwner = this@InputActivity
             viewModel = this@InputActivity.viewModel
